274989c0865939760eb9c51d5d99e3ce2f4bf953
chaaals <charlesching0808@gmail.com>
2022-03-23 16:15:55 +0800

    Update: Finished project Pig Game exercise

---
+++ script.js
@@ -0,0 +1,133 @@
+'use strict';
+
+// Variables
+const score0Elmnt = document.querySelector('#score--0');
+const score1Elmnt = document.getElementById('score--1'); // used for ids, faster then querySelector
+
+const current0Elmn = document.getElementById('current--0');
+const current1Elm = document.getElementById('current--1');
+const diceImage = document.querySelector('.dice');
+const btnDiceRoll = document.querySelector('.btn--roll');
+const btnNewGame = document.querySelector('.btn--new');
+const btnHold = document.querySelector('.btn--hold');
+const playerOne = document.querySelector('.player--0');
+const playerTwo = document.querySelector('.player--1');
+
+let scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
+
+// Game functions
+const diceRoll = function () {
+  let diceNumber = Math.trunc(Math.random() * 6 + 1);
+  return diceNumber;
+};
+// hiding dice image upon initialization
+const diceHidden = function () {
+  diceImage.classList.add('hidden');
+};
+// showing dice image on click event
+const diceShown = function () {
+  diceImage.classList.remove('hidden');
+};
+// accessing rolled dice image
+const displayDiceImage = function (rolledNumber) {
+  diceImage.src = `dice-${rolledNumber}.png`;
+};
+
+const playerOneActive = function (activePlayer) {
+  playerTwo.classList.toggle('player--active');
+  playerOne.classList.toggle('player--active');
+  return (activePlayer = activePlayer === 0 ? 1 : 0);
+};
+
+const playerTwoActive = function (activePlayer) {
+  playerOne.classList.toggle('player--active');
+  playerTwo.classList.toggle('player--active');
+  return (activePlayer = activePlayer === 0 ? 1 : 0);
+};
+
+const activePlayerTextContent = function (activePlayer, currentScore) {
+  document.getElementById(`current--${activePlayer}`).textContent =
+    currentScore;
+};
+
+const gameReset = function (activePlayer) {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+
+  if (playerTwo.classList.contains('player--active')) {
+    playerTwo.classList.remove('player--active');
+    playerOne.classList.add('player--active');
+  }
+
+  initScoreElmntValue();
+  diceHidden();
+  activePlayerTextContent(activePlayer, currentScore);
+};
+
+const initScoreElmntValue = function () {
+  score0Elmnt.textContent = 0; // type coercion happens
+  score1Elmnt.textContent = 0;
+};
+
+initScoreElmntValue();
+diceHidden();
+
+// Game logic
+btnDiceRoll.addEventListener('click', function () {
+  let rolledDice = diceRoll();
+  console.log(rolledDice);
+  displayDiceImage(rolledDice);
+  diceShown();
+  if (rolledDice !== 1) {
+    currentScore += rolledDice;
+    activePlayerTextContent(activePlayer, currentScore);
+  } else {
+    if (activePlayer === 0) {
+      scores[activePlayer] += currentScore;
+      score0Elmnt.textContent = scores[activePlayer];
+      currentScore = 0;
+
+      activePlayerTextContent(activePlayer, currentScore);
+      //   console.log(scores);
+      activePlayer = playerTwoActive(activePlayer);
+      //   activePlayer = activePlayer === 0 ? 1 : 0;
+    } else {
+      scores[activePlayer] += currentScore;
+      score1Elmnt.textContent = scores[activePlayer];
+      currentScore = 0;
+
+      activePlayerTextContent(activePlayer, currentScore);
+      //   console.log(scores);
+      activePlayer = playerOneActive(activePlayer);
+      //   activePlayer = activePlayer === 0 ? 1 : 0;
+    }
+  }
+});
+
+btnHold.addEventListener('click', function () {
+  scores[activePlayer] += currentScore;
+  document.getElementById(`score--${activePlayer}`).textContent =
+    scores[activePlayer];
+  currentScore = 0;
+
+  activePlayerTextContent(activePlayer, currentScore);
+
+  if (scores[activePlayer] >= 100) {
+    alert(
+      `Player ${scores[0] > scores[1] ? 'Player 1 Wins!' : 'Player 2 Wins!'}`
+    );
+  } else {
+    if (activePlayer === 0) {
+      activePlayer = playerTwoActive(activePlayer);
+    } else {
+      activePlayer = playerTwoActive(activePlayer);
+    }
+  }
+});
+
+btnNewGame.addEventListener('click', function () {
+  gameReset(activePlayer);
+});
